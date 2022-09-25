e5dbf95d654f316e6766caa7bd24ef4f64e0a709
MousyBlue <70676808+MousyBlue@users.noreply.github.com>
2022-09-25 22:58:05 +0000

    idk but this is the nearly done but ugly version

--- nav.js
+++ nav.js
@@ -18,7 +18,6 @@ var dropdownArrow = document.getElementsByClassName("arrow");
 var i;
 for (i = 0; i < dropdownArrow.length; i++) {
   dropdownArrow[i].addEventListener("click", function() {
-    this.classList.toggle("active");
     var dropdownAContent = this.nextElementSibling;
     if (dropdownAContent.style.height === "auto") {
       dropdownAContent.style.height = "0px";
@@ -29,6 +28,25 @@ for (i = 0; i < dropdownArrow.length; i++) {
       dropdownAContent.style.height = "auto";
       dropdownAContent.style.padding = "15px";
       dropdownAContent.style.opacity = "1";
+      this.style.transform = "rotate(180deg)";
+    }
+  });
+}
+
+var dropdownArrow2 = document.getElementsByClassName("arrow2");
+var i;
+for (i = 0; i < dropdownArrow2.length; i++) {
+  dropdownArrow2[i].addEventListener("click", function() {
+    var dropdownA2Content = this.nextElementSibling;
+    if (dropdownA2Content.style.height === "auto") {
+      dropdownA2Content.style.height = "0px";
+      dropdownA2Content.style.padding = "0px";
+      dropdownA2Content.style.opacity = "0";
+      this.style.transform = "rotate(0deg)";
+    } else {
+      dropdownA2Content.style.height = "auto";
+      dropdownA2Content.style.padding = "15px";
+      dropdownA2Content.style.opacity = "1";
       this.style.transform = "rotate(180deg)";
     }
   });
